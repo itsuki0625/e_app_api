bffd1f88b0ce25224a4194ba75f4180e39341774
itsuki0625 <itshki@keio.jp>
2023-10-05 04:08:45 +0900

    model change

--- app/feedback/models/models.py
+++ app/feedback/models/models.py
@@ -6,11 +6,17 @@ from datetime import datetime
 from . import db
 
 #CREATE TABLE testmodel (id INT PRIMARY KEY , name TEXT NOT NULL, email VARCHAR(255) UNIQUE);
-class testmodel(db.Model):
-    __tablename__ = 'testmodel'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
-    age = db.Column(db.Integer, nullable=False)
+class Movies(db.Model):
+    __tablename__ = 'movies'
+    movie_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), unique=True, nullable=False) #動画タイトル
+    description = db.Column(db.String(255), nullable=False) #動画説明
+    thumbnail_url = db.Column(db.String(255), nullable=False) #サムネイルURL
+    movie_url = db.Column(db.String(255), nullable=False) #動画URL
+    short_movie_url = db.Column(db.String(255), nullable=False) #short動画URL
+    short_movie_counter = db.Column(db.Integer, nullable=False) #short動画再生回数
+    buy_counter = db.Column(db.Integer, nullable=False) #購入回数
+    auther_id = db.Column(db.Integer, nullable=False) #投稿者
     created_at = db.Column(db.DateTime, nullable=False)
     updated_at = db.Column(db.DateTime, nullable=False)
 
